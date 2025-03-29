9779be7eaa5657bcf03eacac077d69559679dfc6
AlefSillva <alef.santos@al.infnet.edu.br>
2025-03-29 20:32:10 -0300

    Conta bancária simples

---
+++ src/assessment/projeto9/ContaBancaria.java
@@ -0,0 +1,32 @@
+package assessment.projeto9;
+
+public class ContaBancaria {
+    String titular;
+    private double saldo;
+
+    public ContaBancaria(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    // ----------Métodos de Acesso----------
+    void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        } else {
+            System.out.println("Valor inválido para depósito.");
+        }
+    }
+
+    void sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+        } else {
+            System.out.println("Valor inválido para saque.");
+        }
+    }
+
+    double exibirSaldo() {
+        return saldo;
+    }
+}
