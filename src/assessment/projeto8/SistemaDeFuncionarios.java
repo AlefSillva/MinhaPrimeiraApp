8db78b01962423cce82251487938096305c36c39
AlefSillva <alef.santos@al.infnet.edu.br>
2025-03-29 20:30:42 -0300

    criando um sistema de funcionários

---
+++ src/assessment/projeto8/SistemaDeFuncionarios.java
@@ -0,0 +1,13 @@
+package assessment.projeto8;
+
+public class SistemaDeFuncionarios {
+    public static void main(String[] args) {
+        // Criando instâncias Gerente e Estagiario
+        Funcionario gerente = new Gerente("Carlos", 5000);
+        Funcionario estagiario = new Estagiario("Ana", 1500);
+
+        // Exibindo os salários
+        System.out.println("Salário do Gerente: " + gerente.calcularSalario());
+        System.out.println("Salário do Estagiário: " + estagiario.calcularSalario());
+    }
+}
