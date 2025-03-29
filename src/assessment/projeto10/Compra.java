0280f38e8f33f1e357df153de78fe62e61de23fd
AlefSillva <alef.santos@al.infnet.edu.br>
2025-03-29 20:31:54 -0300

    criando a classe Compra

---
+++ src/assessment/projeto10/Compra.java
@@ -0,0 +1,13 @@
+package assessment.projeto10;
+
+public class Compra {
+    String produto;
+    int quantidade;
+    double precoUnitario;
+
+    public Compra(String produto, int quantidade, double precoUnitario) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+}
