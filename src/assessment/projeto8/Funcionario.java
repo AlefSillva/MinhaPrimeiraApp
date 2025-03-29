65f102a4f2e021a4acc070cf3b03da809c022594
AlefSillva <alef.santos@al.infnet.edu.br>
2025-03-29 20:29:47 -0300

    Criação da classe funcionário

---
+++ src/assessment/projeto8/Funcionario.java
@@ -0,0 +1,15 @@
+package assessment.projeto8;
+
+public class Funcionario {
+    String nome;
+    double salarioBase;
+
+    public Funcionario(String nome, double salarioBase) {
+        this.nome = nome;
+        this.salarioBase = salarioBase;
+    }
+
+    public double calcularSalario() {
+        return salarioBase;
+    }
+}
